3e3b62bbb4ad1df3efa6dba749cbb10746ab7ff9
zStupan <ziga.stupan@gmail.com>
2022-12-27 00:13:04 +0100

    reordered includes

--- src/NumAssociationRules.jl
+++ src/NumAssociationRules.jl
@@ -6,11 +6,11 @@ export Attribute, build_rule, feature_position, mine, evaluate, cut_point,
         supp_conf, feature_borders, calculate_fitness, Feature, load_dataset,
         preprocess_data, problem_dimension, Rule
 
-include("preprocess.jl")
-include("evaluate.jl")
 include("feature.jl")
 include("attribute.jl")
 include("rule.jl")
+include("preprocess.jl")
 include("build_rule.jl")
+include("evaluate.jl")
 
 end
