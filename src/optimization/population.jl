0701e4f39d98c385582c348756e26dc904248c6e
zStupan <ziga.stupan@gmail.com>
2023-01-01 11:18:11 +0100

    added/updated type annotations

--- src/optimization/population.jl
+++ src/optimization/population.jl
@@ -1,3 +1,3 @@
-function initpopulation(popsize::Int64, problem::Problem, rng::MersenneTwister)
+function initpopulation(popsize::Int64, problem::Problem, rng::AbstractRNG)
     return problem.lowerinit .+ rand!(rng, zeros(popsize, problem.dimension)) .* (problem.upperinit - problem.lowerinit)
 end
